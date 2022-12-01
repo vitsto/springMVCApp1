fd8954ac16f05e6e9523dd4500266185aedd461f
vitsto <stolyarov.vitalij@list.ru>
2022-12-01 13:38:25 +1000

    jdbc

--- src/main/java/ru/stolyarov/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/stolyarov/springcourse/controllers/PeopleController.java
@@ -1,8 +1,10 @@
 package ru.stolyarov.springcourse.controllers;
 
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.stolyarov.springcourse.dao.PersonDao;
 import ru.stolyarov.springcourse.models.Person;
@@ -10,7 +12,6 @@ import ru.stolyarov.springcourse.models.Person;
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
-
     private final PersonDao personDAO;
 
     @Autowired
@@ -36,7 +37,11 @@ public class PeopleController {
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "people/new";
+        }
+
         personDAO.save(person);
         return "redirect:/people";
     }
@@ -48,7 +53,11 @@ public class PeopleController {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult, @PathVariable("id") int id) {
+        if (bindingResult.hasErrors()) {
+            return "people/edit";
+        }
+
         personDAO.update(id, person);
         return "redirect:/people";
     }
